8ed72da923fb0d13d38f4a22d63916d07cfde634
Mark Needham <m.h.needham@gmail.com>
2023-07-31 07:34:57 +0100

    pull out functions

--- examples/jupyter.mjs
+++ examples/jupyter.mjs
@@ -5,32 +5,96 @@ import {
 } from '../drive_pinot.mjs'
 
 
+async function executeQuery(page) {
+    await page.keyboard.down('Meta');
+    await page.keyboard.press('Enter');
+    await page.keyboard.up('Meta'); 
+}
+
+async function waitForQueryToFinish(page) {
+    await page.waitForFunction(
+        () => document.querySelector('.jp-mod-active .jp-InputPrompt.jp-InputArea-prompt').textContent.includes('*'),
+        {timeout: 5000}
+    );
+  
+    await page.waitForFunction(
+        () => !document.querySelector('.jp-mod-active .jp-InputPrompt.jp-InputArea-prompt').textContent.includes('*'),
+        {timeout: 60000}
+    );
+}
+
 async function run() {
     const zoomLevel = 250;
-    const token = "a17191c48278aaf729b8f811ea30094a43685a730991bac4";
+    const token = "9f2ae06c9c9640fc75a1f7b013486924e6a15b76ab2248c4";
     const { browser, page } = await launchBrowseAndOpenPage({
-        url: `http://localhost:8888/doc/workspaces/auto-B/tree/SentimentAnalysis.ipynb?token=${token}&simpleInterface=true`, 
+        url: `http://localhost:8888/doc/tree/LLMOwnLaptop2.ipynb?token=${token}`, 
         zoomLevel: `${zoomLevel}%`
     });
-
+        
+    await page.waitForSelector('.jp-Notebook');
     await new Promise(r => setTimeout(r, 1000))
 
-    await page.keyboard.down('Meta');
-    await page.keyboard.down('Shift'); 
-    await page.keyboard.press('f');
-    await page.keyboard.up('Shift'); 
-    await page.keyboard.up('Meta');
+    // await page.keyboard.down('Meta');
+    // await page.keyboard.down('Shift'); 
+    // await page.keyboard.press('F');
+    // await page.keyboard.up('Shift'); 
+    // await page.keyboard.up('Meta');
 
-    await new Promise(r => setTimeout(r, 3000))
+    // await page.keyboard.press('Escape');
+    // await new Promise(r => setTimeout(r, 500))
+    // await page.keyboard.press('b');
+    // await page.keyboard.press('Enter');
 
-    const delay = ms => new Promise(res => setTimeout(res, ms));
+    // await page.keyboard.type('print("Hello, world!")', {delay: 100});
 
-    for (let i = 0; i < 5; i++) {
-        await page.keyboard.press('ArrowDown');
-        await delay(200); // Sleep for 100 milliseconds
-    }
+    // await page.keyboard.down('Meta');
+    // await page.keyboard.press('Enter');
+    // await page.keyboard.up('Meta');
+
+    await page.keyboard.press('Escape');
+    await new Promise(r => setTimeout(r, 500))
+    await page.keyboard.press('b');
+    await page.keyboard.press('Enter');
+
+    let code = `import time
+time.sleep(5)`;
+
+    await page.keyboard.type(code, {delay: 100});
+
+    await executeQuery(page)
+    await waitForQueryToFinish(page);
+
+    await page.keyboard.press('Escape');
+    await new Promise(r => setTimeout(r, 500))
+    await page.keyboard.press('b');
+    await page.keyboard.press('Enter');
+
+    await page.keyboard.type('print("Hello, world!")', {delay: 100});
+
+    await executeQuery(page);
+    await waitForQueryToFinish(page);
+
+    await page.keyboard.press('Escape');
+    await new Promise(r => setTimeout(r, 500))
+    await page.keyboard.press('b');
+    await page.keyboard.press('Enter');
+
+    await page.keyboard.type('print("Hello, Arya!")', {delay: 100});
+
+    await executeQuery(page)
+    await waitForQueryToFinish(page);
+
+
+    // await new Promise(r => setTimeout(r, 3000))
+
+    // const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    // for (let i = 0; i < 5; i++) {
+    //     await page.keyboard.press('ArrowDown');
+    //     await delay(200); // Sleep for 100 milliseconds
+    // }
     
-    await scrollJupyter(page, 1000)
+    // await scrollJupyter(page, 1000)
 
     // await browser.close();
 }
